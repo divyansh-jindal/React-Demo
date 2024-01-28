acdce2da064a0c71b8370c270a03c80a43013624
Divyansh Jindal <divyanshjindal18@gmail.com>
2024-01-28 17:11:44 +0530

    filter button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,29 @@
 import RestrauntCard from "./RestrauntCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestraunts, setListOfRestraunts] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestraunts.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestraunts(filteredList);
+            // console.log(listOfRestraunts);
+          }}
+        >
+          Top Rated Restraunts
+        </button>
+      </div>
       <div className="res-container">
         {
           /* <RestrauntCard resData={resList[0]} /> */
-          resList.map((restraunt) => (
+          listOfRestraunts.map((restraunt) => (
             <RestrauntCard key={restraunt.info.id} resData={restraunt} />
           ))
         }
