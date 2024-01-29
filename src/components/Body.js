ec47fec843ebb6b0de8ed9420d3b5e158ba490a4
Divyansh Jindal <divyanshjindal18@gmail.com>
2024-01-29 14:37:15 +0530

    added shimmer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,30 @@
 import RestrauntCard from "./RestrauntCard";
 import resList from "../utils/mockData";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestraunts, setListOfRestraunts] = useState(resList);
+  const [listOfRestraunts, setListOfRestraunts] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch();
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const json = await data.json();
 
     console.log(json);
+    setListOfRestraunts(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
+  if (listOfRestraunts.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="filter">
