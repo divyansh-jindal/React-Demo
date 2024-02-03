91eb6d0223e2939e3ed4ba7f450176004c64f6ee
Divyansh Jindal <divyanshjindal18@gmail.com>
2024-02-03 10:27:30 +0530

    Menu

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import RestaurantMenu from "./components/RestaurantMenu";
 
 // const parent = React.createElement(
 //   "div",
@@ -63,6 +64,10 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
       },
     ],
     errorElement: <Error />,
---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+const RestaurantMenu = () => {
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+  };
+
+  return (
+    <div className="menu">
+      <h1>Name of the Restauran</h1>
+      <h2>Menu</h2>
+      <ul>
+        <li>Biryani</li>
+        <li>Burgers</li>
+        <li>Diet Coke</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
