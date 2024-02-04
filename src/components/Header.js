0279a767b6ac05941dead1cc112dae0dd70dd6d3
Divyansh Jindal <divyanshjindal18@gmail.com>
2024-02-04 18:05:48 +0530

    added online status in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
+  const onlineStatus = useOnlineStatus();
 
   return (
     <div className="header">
@@ -12,6 +14,10 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>
+            Online Status:{" "}
+            {onlineStatus ? "✅" : "                                     "}
+          </li>
           <li>
             <Link to="/">Home</Link>
           </li>
